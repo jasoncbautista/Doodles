0c0555db9f6f6790e8afbbeff3a45d5958a64a65
Jason B <bjason@gmail.com>
2015-07-13 01:21:15 -0700

    For input .

--- MiniChallenges/001_UniqueChars/unique_chars.c
+++ MiniChallenges/001_UniqueChars/unique_chars.c
@@ -3,12 +3,14 @@
 
 
 main(){
-    char sampleInput[] = "one";
+    char sampleInput[] = "abcone";
     int len = sizeof(sampleInput);
 
     printf("%i = len \n", len);
-    for(int i = 0; i < len; i++){
+    for(int i = 0; i < len -1 ; i++){
+        int charValue  = (int)  sampleInput[i] - 97;
         printf("%c \n", sampleInput[i]);
+        printf("%i \n", charValue);
 
     }
 
