44a8f0f294b43fba655d082bc64bb71166bbdc7a
Jason B <bjason@gmail.com>
2015-07-13 02:23:09 -0700

    Seen beofre in C.

--- MiniChallenges/001_UniqueChars/unique_chars.c
+++ MiniChallenges/001_UniqueChars/unique_chars.c
@@ -8,26 +8,36 @@ typedef int bool;
 
 
 int main(){
-    char sampleInput[] = "abcone";
+    char sampleInput[] = "abconae";
     int len = sizeof(sampleInput);
     bool *seenBefore;
     int seenBeforeLen = 10;
     seenBefore = (bool*)  calloc(seenBeforeLen, sizeof(bool)  );
-    seenBefore[2] = 1;
 
     printf("%i = len \n", len);
     for(int i = 0; i < len -1 ; i++){
         int charValue  = (int)  sampleInput[i] - 97;
         printf("%c \n", sampleInput[i]);
         printf("%i \n", charValue);
+
+        if(seenBefore[charValue] == true){
+            printf("SEEN BEFORE! \n\n\n");
+            break;
+        }
+        seenBefore[i] = true;
     }
 
-    printf("\n");
     print_bools(seenBeforeLen, seenBefore);
 
 }
 
+
+/*
+ * Simple function to help print out boolean array.
+ */
 int print_bools(int seenBeforeLen, bool* seenBefore){
+
+    printf("\n");
     for(int j = 0; j < seenBeforeLen ; j++){
         if(seenBefore[j] == true){
             printf("YES!\n");
