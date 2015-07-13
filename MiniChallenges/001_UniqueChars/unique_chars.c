258e127c6621c1b04a182fcd4b591ade45ac840a
Jason B <bjason@gmail.com>
2015-07-13 01:47:52 -0700

    Boolean array initialized.

--- MiniChallenges/001_UniqueChars/unique_chars.c
+++ MiniChallenges/001_UniqueChars/unique_chars.c
@@ -1,24 +1,36 @@
 #include <stdio.h>
+#include <stdlib.h>
+
+typedef int bool;
+
+#define true 1
+#define false 0
 
 
-
-main(){
+int main(){
     char sampleInput[] = "abcone";
     int len = sizeof(sampleInput);
+    bool *seenBefore;
+    int seenBeforeLen = 4;
+    seenBefore = (bool*)  calloc(seenBeforeLen, sizeof(bool)  );
+    
+
 
     printf("%i = len \n", len);
     for(int i = 0; i < len -1 ; i++){
         int charValue  = (int)  sampleInput[i] - 97;
         printf("%c \n", sampleInput[i]);
         printf("%i \n", charValue);
-
     }
 
-
-
     printf("\n");
-    
 
 
+    for(int j = 0; j < seenBeforeLen ; j++){
+        printf("%i\n", seenBefore[j]);
+    }
 
 }
+
+
+
